0a8ffe6067b0e7365d30898cd8c8011a568dbd61
tonymachu <135691150+tonymachu@users.noreply.github.com>
2023-06-07 10:14:38 +0200

    Add files via upload

---
+++ ejercicio4/src/test/java/com/curso/jenkins/ejercicio4/Ejercicio4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.jenkins.ejercicio4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
