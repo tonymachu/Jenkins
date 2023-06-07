e56b6fc23f9ff6e21f3c299d0c42751addd8cc2a
tonymachu <135691150+tonymachu@users.noreply.github.com>
2023-06-07 13:07:48 +0200

    Add files via upload

---
+++ ejercicio5/src/test/java/com/curso/jenkins/integracion/ejercicio5/Ejercicio5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.curso.jenkins.integracion.ejercicio5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ejercicio5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
